d0f454f08a4be3b228bc60798453295bcefc78e6
michelle raouf <michelle.raouf@outlook.com>
2024-02-22 00:08:27 +0200

    fix publish

--- TComposeDatePicker/build.gradle.kts
+++ TComposeDatePicker/build.gradle.kts
@@ -26,7 +26,7 @@ afterEvaluate {
         publications.create<MavenPublication>("release") {
             groupId = "io.github.the-best-is-best"
             artifactId = "compose_date_time_picker"
-            version = "2.0.0"
+            version = "2.0.1"
             from(components["release"])
 
 
