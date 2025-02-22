e926771aa8caf58861fcc6b97868029785e2523f
Michelle Raouf <michelle.raouf@outlook.com>
2025-02-22 15:21:54 +0200

    Refactor ConfigDateTimePicker location
    The `ConfigDateTimePicker` class was moved to a new location within the project.

--- TComposeDateTimePicker/src/commonMain/kotlin/io/github/tcompose_date_picker/config/ConfigDateTimePicker.kt
+++ TComposeDateTimePicker/src/commonMain/kotlin/io/github/tcompose_date_picker/config/ConfigDateTimePicker.kt
@@ -1,13 +1,10 @@
 package io.github.tcompose_date_picker.config
 
-import androidx.compose.material3.DatePickerFormatter
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.sp
-import io.github.tcompose_date_picker.config.ConfigDatePicker
-import io.github.tcompose_date_picker.config.ConfigTimePicker
 
 @OptIn(ExperimentalMaterial3Api::class)
 class ConfigDateTimePicker(
