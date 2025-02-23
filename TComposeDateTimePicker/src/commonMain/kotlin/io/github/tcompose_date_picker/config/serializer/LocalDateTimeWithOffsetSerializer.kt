8086fb6c273c421c416862f38f77f76de8aba23a
Michelle Raouf <michelle.raouf@52ndsolution.net>
2025-02-23 18:55:21 +0200

    feat: add kotlinx-serialization and update version
    - Add `kotlinx-serialization-json` dependency with version 1.7.3.
    - Add a new serializer `LocalDateTimeWithOffsetSerializer` for handling `LocalDateTime` objects.
    - Update the library version to 3.1.2.

---
+++ TComposeDateTimePicker/src/commonMain/kotlin/io/github/tcompose_date_picker/config/serializer/LocalDateTimeWithOffsetSerializer.kt
@@ -0,0 +1,27 @@
+package io.github.tcompose_date_picker.config.serializer
+
+import io.github.tcompose_date_picker.extensions.toIsoStringWithOffset
+import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+
+object LocalDateTimeWithOffsetSerializer : KSerializer<LocalDateTime> {
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("LocalDateTime", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: LocalDateTime) {
+        encoder.encodeString(value.toIsoStringWithOffset())
+    }
+
+    override fun deserialize(decoder: Decoder): LocalDateTime {
+        val instant = Instant.parse(decoder.decodeString()) // Parse Instant
+        return instant.toLocalDateTime(TimeZone.currentSystemDefault()) // Convert back
+    }
+}
